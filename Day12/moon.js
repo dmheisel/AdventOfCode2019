68c63d11ea4b1e9dc8dcc7375d06af7a91264d35
dmheisel <david.heisel@granicus.com>
2019-12-31 10:59:43 -0600

    moon class created/updated for day 12 part 1

--- Day12/moon.js
+++ Day12/moon.js
@@ -4,23 +4,33 @@ class Moon {
         this._position = position || {x: 0, y: 0, z: 0}
         this._velocity = velocity || {x: 0, y: 0, z: 0}
     }
-
+    //apply the moons velocity to update its current position - done on each time tick in system.
     applyVelocity() {
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
         this.position.z += this.velocity.z;
     }
+    //updates velocity with new values -- given from gravity calculations.
     updateVelocity(newVelocity) {
         this.velocity.x += newVelocity.x;
         this.velocity.y += newVelocity.y;
         this.velocity.z += newVelocity.z;
     }
+
+    //energy calculations - could combine into one? left separate in case pot or kin energy is every needed
     calculatePotentialEnergy() {
         const position = this.position;
         return Math.abs(position.x) + Math.abs(position.y) + Math.abs(position.z)
     }
+    calculateKineticEnergy() {
+        const velocity = this.velocity;
+        return Math.abs(velocity.x) + Math.abs(velocity.y) + Math.abs(velocity.z)
+    }
+    calculateTotalEnergy() {
+        return this.calculatePotentialEnergy() * this.calculateKineticEnergy()
+    }
 
-    
+    //getters and setters
     get name() {
         return this._name
     }
--- Day12/moon.test.js
+++ Day12/moon.test.js
@@ -19,9 +19,20 @@ test("Moon update velocity function updates velocity by adding provided values",
     expect(io.velocity).toEqual({x: 4, y: -1, z: 8})
 })
 describe("Moon total energy calculations work correctly.", () => {
+    const io = new Moon("Io", { x: 2, y: 1, z: -3 }, { x: -3, y: -2, z: 1 })
+    const ganymede = new Moon("Ganymede", {x: 3, y: -6, z: 1}, {x: 3, y: 2, z: -3})
+
     test("Moon potential energy method provides total potential energy (sum of abs value of positions)", () => {
-        const io = new Moon("Io", { x: -5, y: 17, z: -13 })
-        expect(io.calculatePotentialEnergy()).toBe(35)
+        expect(io.calculatePotentialEnergy()).toBe(6)
+        expect(ganymede.calculatePotentialEnergy()).toBe(10)
+    })
+    test("Moon kinetic energy calculation provides total kinetic energy (sum of abs value of velocity)", () => {
+        expect(io.calculateKineticEnergy()).toBe(6)
+        expect(ganymede.calculateKineticEnergy()).toBe(8)
+    })
+    test("Moon total energy calculation provides potential energy * kinetic energy", () => {
+        expect(io.calculateTotalEnergy()).toBe(36)
+        expect(ganymede.calculateTotalEnergy()).toBe(80)
     })
     
 })
